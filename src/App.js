972557856282426cf008a3c2b9d85bdd68532ba3
Kevin Hylant <kevin.hylant@gmail.com>
2020-04-16 22:10:42 -0400

    add confetti, general styling, some queestions, styling, heavily hard-coded

--- src/App.js
+++ src/App.js
@@ -1,26 +1,137 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import cx from 'classnames/bind';
+import { useForm } from 'react-hook-form'
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+  useParams,
+  Redirect,
+} from "react-router-dom";
+import useWindowSize from 'react-use/lib/useWindowSize'
+import ReactConfetti from 'react-confetti'
+import { images } from './images';
+import './App.scss';
 
-function App() {
+const questions = [{
+  answer: 'dragon',
+  image: 'dragon'
+}]
+
+const Confetti = () => {
+  const { width, height } = useWindowSize()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ReactConfetti
+      width={width}
+      height={height}
+    />
+  )
 }
+
+const Home = ({ }) => {
+  const { register, handleSubmit, errors } = useForm()
+  const [isCorrect, setCorrect] = useState(false);
+  const [isIncorrect, setIncorrect] = useState(false);
+  const [incorrectCount, setIncorrectCount] = useState(0);
+  const { questionIndex } = useParams();
+
+  const onSubmit = ({ guess }) => {
+    const answer = questions[questionIndex].answer;
+    const isIncorrect = answer !== guess.toLowerCase();
+    if (isIncorrect) {
+      setCorrect(false); // needed?
+      setIncorrect(true);
+      setIncorrectCount(incorrectCount + 1);
+      console.log('INCORRECT!');
+    } else {
+      setCorrect(true);
+      setIncorrect(false);
+      setIncorrectCount(0);
+      console.log('CORRECT!');
+    }
+    console.log({ isIncorrect, questionIndex });
+  }
+  console.log({ incorrectCount })
+  return (
+    <>
+      <div className="guess">
+        <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+          <span className="form__label">Guess here</span>
+          <div className="form__fields">
+            <input
+              type="text"
+              name="guess"
+              autoComplete="off"
+              ref={register({ required: true })}
+              onChange={() => {
+                setIncorrect(false);
+              }}
+            />
+            <input type="submit" value="👉 🔴" />
+          </div>
+          {errors.guess && (
+            <p className="validation-error">Try a little harder... literally anything is better than your guess.</p>
+          )}
+        </form>
+        <div className={cx('heckling', ({
+          'heckling--hidden': !isIncorrect,
+        }))}>
+          <img src={images.heckling.chrisFarley} />
+        </div>
+      </div>
+      <div className="question">
+        <div className="question__image-container">
+          <img src={images.questions.dragon} />
+        </div>
+      </div>
+      {isCorrect && <Confetti />}
+    </>
+  )
+}
+
+
+const App = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <Router>
+      <div className="app">
+        <nav className="nav">
+          <h1 className="nav__title">
+            <Link to="/">Pun fun</Link>
+          </h1>
+          <h2>
+            <Link to="/about">About</Link>
+          </h2>
+          <button
+            className={cx('nav__hamburger hamburger hamburger--collapse', {
+              'is-active': isMenuOpen,
+            })}
+            onClick={() => setMenuOpen(!isMenuOpen)}
+            type="button"
+            aria-label="Menu"
+            aria-controls="navigation"
+          >
+            <span className="hamburger-box">
+              <span className="hamburger-inner"></span>
+            </span>
+          </button>
+        </nav>
+        <div className="content">
+          <Switch>
+            <Route path="/:questionIndex">
+              <Home />
+            </Route>
+            <Route path="/">
+              <Redirect to={{ pathname: '/0' }} />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/images.js
@@ -0,0 +1,16 @@
+import chrisFarley from './images/heckling/chrisFarley.gif';
+import jamesCorden from './images/heckling/jamesCorden.gif';
+import neilPatrickHarris from './images/heckling/neilPatrickHarris.gif';
+
+import dragon from './images/questions/dragon.jpg';
+
+export const images = {
+  heckling: {
+    chrisFarley,
+    jamesCorden,
+    neilPatrickHarris,
+  },
+  questions: {
+    dragon,
+  },
+};
