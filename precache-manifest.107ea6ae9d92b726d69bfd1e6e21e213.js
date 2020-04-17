9e2f2a38c26d78237ca9f522fa9e9e5911b7b4da
Kevin Hylant <kevin.hylant@gmail.com>
2020-04-16 22:12:03 -0400

    Updates

---
+++ precache-manifest.107ea6ae9d92b726d69bfd1e6e21e213.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad5b5668be2665a44564fb540ae136c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a14b5d86aeebcfccce6",
+    "url": "/static/css/main.84e79a00.chunk.css"
+  },
+  {
+    "revision": "74f16b039c540b8fce58",
+    "url": "/static/js/2.4ab9d7aa.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.4ab9d7aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a14b5d86aeebcfccce6",
+    "url": "/static/js/main.dd04e12e.chunk.js"
+  },
+  {
+    "revision": "0fab776da41d98c64114",
+    "url": "/static/js/runtime-main.38043156.js"
+  },
+  {
+    "revision": "67ab40477ff4f874c3654fb977ede1bd",
+    "url": "/static/media/chrisFarley.67ab4047.gif"
+  },
+  {
+    "revision": "8b0ef6862ba8273cd920901293ee9998",
+    "url": "/static/media/dragon.8b0ef686.jpg"
+  },
+  {
+    "revision": "a9910605c2af0755fc1291223dad81f3",
+    "url": "/static/media/jamesCorden.a9910605.gif"
+  },
+  {
+    "revision": "0c746bacf22783d311952c1d74d81229",
+    "url": "/static/media/neilPatrickHarris.0c746bac.gif"
+  }
+]);
